42ac9f57a6a0589bfba7a1770e6916004883db33
vfrsvidal <vfrsvidal@outlook.com>
2014-04-02 19:16:46 -0300

    Revert "first commit"
    This reverts commit 87b8b0cdad49040ad2827f760a6ffbc0420cf6e6.

--- q1.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-      printf("OLA MUNDO");
-      return (0);
-      }
--- q3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-main()
-{ int numero;
-  printf("Digite o numero");
-  scanf("%d", &numero);
-  printf ("O numero é: %d", numero);
-  return 0;
-  }

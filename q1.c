87b8b0cdad49040ad2827f760a6ffbc0420cf6e6
vfrsvidal <vfrsvidal@outlook.com>
2014-04-02 19:00:09 -0300

    first commit

---
+++ q1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+      printf("OLA MUNDO");
+      return (0);
+      }
---
+++ q2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main()
+                         { 
+int x;
+scanf("%d",&x);
+printf("%d",x);
+ return (0);
+ }
+//O programa repete o numero digitado.
